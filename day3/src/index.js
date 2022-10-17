df1cfcb644206703b4dbade711d0a25af78b1dfd
ganjispandana30 <ganji.spandana@getfareye.com>
2022-10-18 01:56:41 +0530

    done

--- day3/src/index.js
+++ day3/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import App1 from './App1';
+
 import App2 from './App2';
 
 import reportWebVitals from './reportWebVitals';
-import UserCreate from './Components/User';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserCreate />
+    <App2 />
     
   </React.StrictMode>
 );
